02cc102d0bad80984e40363fcab4a3fe2082b677
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-12 16:42:17 +0100

    fai index+show+delete

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const postsRouter = require("./routers/posts");
 
+app.use(express.static("public"));
+
 // Middleware per gestire JSON
 app.use(express.json()); 
 app.use("/posts", postsRouter);
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,12 +1,36 @@
+const posts = require("../data/posts2");
+
+
 // INDEX - Lista di tutti i post
 const index = (req, res) => {
-    res.send("Lista dei post");
+    res.json(posts);
   };
+
   
-  // SHOW - Mostra un singolo post
-  const show = (req, res) => {
-    res.send(`Mostra il post con ID: ${req.params.id}`);
-  };
+// SHOW - Mostra un singolo post
+function show(req, res) {
+    // res.send('visualizza un elemento')
+
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il post tramite id
+    const post = posts.find(post => post.id === id);
+
+    // controlliamo se il parametro inserito esiste
+    if(!post) {
+        // ritorno lo stato di errore 404, non trovato
+        res.status(404);
+
+        // ritorno un messaggio di errore (formato json)
+        return res.json({
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+        // Restituiamolo sotto forma di JSON   
+        res.json(post);
+}
   
   // CREATE - Crea un nuovo post
   const create = (req, res) => {
@@ -18,10 +42,38 @@ const index = (req, res) => {
     res.send(`Aggiornamento del post con ID: ${req.params.id}`);
   };
   
-  // DELETE - Cancella un post
-  const destroy = (req, res) => {
-    res.send(`Cancellazione del post con ID: ${req.params.id}`);
-  };
-  
+
+
+// funzione DESTROY
+function destroy(req, res) {
+    // res.send('Cancella post post' + req.params.id);
+
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il post tramite id
+    const post = posts.find(post => post.id === id);
+
+    // controlliamo se il parametro inserito esiste
+    if(!post) {
+        // ritorno lo stato di errore 404, non trovato
+        res.status(404);
+
+        // ritorno un messaggio di errore (formato json)
+        return res.json({
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    // cancello il post
+    posts.splice(posts.indexOf(post), 1);
+
+    // log di riscontro di check su aggiornamento dati
+    console.log(posts); 
+
+    // ritorno la risposta positiva di avvenuta cancellazione
+    res.sendStatus(204);
+}
   module.exports = { index, show, create, update, destroy };
   
