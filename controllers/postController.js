ca8f4c2272424a9ae207d0a16b6f7b679745ba24
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-13 19:16:47 +0100

    fai logica put

--- controllers/postController.js
+++ controllers/postController.js
@@ -63,9 +63,21 @@ function store(req, res) {
   
   // UPDATE - Aggiorna un post
   const update = (req, res) => {
-    res.send(`Aggiornamento del post con ID: ${req.params.id}`);
-  };
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
   
+    if (!post) {
+      return res.status(404).json({ error: "Not Found", message: "Post non trovato" });
+    }
+  
+    post.title = req.body.title || post.title;
+    post.content = req.body.content || post.content;
+    post.image = req.body.image || post.image;
+    post.tags = req.body.tags || post.tags;
+  
+    console.log("Post aggiornato:", post);
+    res.json(post);
+  };
 
 
 // funzione DESTROY
